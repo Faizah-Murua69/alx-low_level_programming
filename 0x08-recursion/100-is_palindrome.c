55a52a646e47c88a18454789ad2189eaf0944082
Faizah-Murua69 <faizahmurua1@gmail.com>
2023-04-05 10:58:39 -0700

    All Done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,42 +1,60 @@
-#include "main.h"
-int check_pal(char *s, int i, int len);
-int _strlen_recursion(char *s);
-/**
-* is_palindrome - checks if a string is a palindrome
-* @s: string to reverse
-*
-* Return: 1 if it is, 0 it's not
-*/
-int is_palindrome(char *s)
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXLENGTH 31
+
+/*
+  calculate the length of the string
+ */
+int get_string_length(char *str);
+
+/*
+  return 1 if it is palindrome
+  return 0 if it is not palindrome
+ */
+int is_palindrome(char *str);
+
+int main(int argc, char *argv[])
 {
-if (*s == 0)
-return (1);
-return (check_pal(s, 0, _strlen_recursion(s)));
+    char input_string[MAXLENGTH];
+
+    // get user input
+    printf("Enter a string to test: ");
+    scanf("%s", input_string);
+
+    if(is_palindrome(input_string)) {
+        printf("%s is a palindrome\n", input_string);
+    } else {
+        printf("%s is not a palindrome\n", input_string);
+    }
+
+    return 0;
 }
-/**
-* _strlen_recursion - returns the length of a string
-* @s: string to calculate the length of
-*
-* Return: length of the string*/
-int _strlen_recursion(char *s)
+
+int get_string_length(char *str)
 {
-if (*s == '\0')
-return (0);
-return (1 + _strlen_recursion(s + 1));
+    int length = 0;
+    // if it is not '\0' keep moving
+    while(*str != '\0') {
+        str++;
+        length++;
+    }
+    return length;
 }
-/**
-* check_pal - checks the characters recursively for palindrome
-* @s: string to check
-* @i: iterator
-* @len: length of the string
-*
-* Return: 1 if palindrome, 0 if not 
-*/
-int check_pal(char *s, int i, int len)
+
+int is_palindrome(char *str)
 {
-if (*(s + i) != *(s + len - 1))
-return (0);
-if (i >= len)
-return (1);
-return (check_pal(s, i + 1, len - 1));
+    char *head = str;
+    int length = get_string_length(str);
+    char *tail = &str[length - 1];
+
+    // two pointers to check palindrome and no spare space is being used
+    while(head < tail) {
+        if(*head != *tail) {
+            return 0;
+        }
+        head++;
+        tail--;
+    }
+    return 1;
 }
