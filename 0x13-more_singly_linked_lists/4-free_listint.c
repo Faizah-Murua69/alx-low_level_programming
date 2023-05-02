7eecd16bf4968cb888d77a99f1a6884b199aeeb4
Faizah-Murua69 <faizahmurua1@gmail.com>
2023-05-02 12:58:36 -0700

    All

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,18 +1,15 @@
 #include "lists.h"
-
 /**
- * free_listint - frees a linked list
- * @head: head of a list.
- *
- * Return: no return.
- */
+* free_listint - frees a linked list
+* @head: listint_t list to be freed
+*/
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
-
-	while ((temp = head) != NULL)
-	{
-		head = head->next;
-		free(temp);
-	}
+listint_t *temp;
+while (head)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
 }
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,20 +1,20 @@
 #include "lists.h"
-
 /**
- * get_nodeint_at_index - returns the nth node of a linked list
- * @head: head of a list.
- * @index: index of the node.
- *
- * Return: nth node. If node does not exist, returns NULL.
- */
+* get_nodeint_at_index - returns the node at a certain index in a linked
+list
+* @head: first node in the linked list
+* @index: index of the node to return
+*
+* Return: pointer to the node we're looking for, or NULL
+*/
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
-
-	for (i = 0; i < index && head != NULL; i++)
-	{
-		head = head->next;
-	}
-
-	return (head);
+unsigned int i = 0;
+listint_t *temp = head;
+while (temp && i < index)
+{
+temp = temp->next;
+i++;
+}
+return (temp ? temp : NULL);
 }
